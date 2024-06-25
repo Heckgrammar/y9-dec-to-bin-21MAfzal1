84ef91ad80a63ee9c3172a9c6ba2377ed2325f4b
21MAfzal <160749351+21MAfzal1@users.noreply.github.com>
2024-06-25 15:14:31 +0100

    Update Program.cs
    v1.2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,9 +18,48 @@ namespace Y9_DEC_TO_BIN_SKELETON
             int myStringAsInt = Convert.ToInt32(myString); //watch me cast from string to int
 
             //MAIN:  NUMBER CONVERSION PROGRAM
-            Console.WriteLine( numberConversion(480 , 68));
+            Console.WriteLine(numberConversion(480, 68));
+            string one = "animal";
+            string two = "123";
+            string combined = one + two;
+            Console.WriteLine(combined);
             //CODE GOES HERE
+
+            string binaryNumber = " ";
+
+            Console.WriteLine("Enter a number");
+            int number = Console.Read();
+            int remainder = number % 2;
+            while (remainder != 0) ;
+            { 
+            remainder = number % 2;
+            binaryNumber = " ";
+            string remainder2 = remainder.ToString();
+            binaryNumber = binaryNumber + remainder2;
+            number = number / 2;
         }
+            while (remainder != 1) ;
+            {
+                
+                 binaryNumber = " ";
+                string remainder2 = remainder.ToString();
+                binaryNumber = binaryNumber + remainder2;
+                number = number / 2;
+            }
+             while (remainder != 0) ;
+            { 
+            remainder = number % 2;
+            binaryNumber = " ";
+            string remainder2 = remainder.ToString();
+            binaryNumber = binaryNumber + remainder2;
+            number = number / 2;
+        } remainder = number % 2;
+            Console.WriteLine(binaryNumber);
+        }
+
+
+
+
 
         //static void means the function will not return a value so it does not need a data type 
         //...this function DOES return a value so the method must have a data type
@@ -28,8 +69,12 @@ namespace Y9_DEC_TO_BIN_SKELETON
             int calc = number + numberbase;
             string result = calc.ToString();
 
+            return result; //REMOVE THE RED LINE!
 
-            return  result ; //REMOVE THE RED LINE!
+
+            {
+               
+            }
         }
     }
 }
